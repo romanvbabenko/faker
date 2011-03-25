0c7110568a4156cb863d436767cfc2dab010dc4e
Roman V. Babenko <romanvbabenko@gmail.com>
2011-03-25 15:31:27 +0200

    [+] Faker generators

---
+++ lib/generators/faker/install_generator.rb
@@ -0,0 +1,22 @@
+module Faker
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "Creates a Faker initializer and copy locale files to your application."
+      class_option :orm
+
+      def copy_initializer
+        template "faker.rb", "config/initializers/faker.rb"
+      end
+
+      def copy_locale
+        copy_file "../../locales/en.yml", "config/locales/faker.en.yml"
+      end
+
+      def show_readme
+        readme "README" if behavior == :invoke
+      end
+    end
+  end
+end
